ecefdb7e8996afb1d01d98d72591bea69fdf246f
Jesus <jveracal553@gmail.com>
2022-02-10 01:34:08 -0500

    adding grid components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import GridCanchita from "./components/GridCanchita";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Canchita from "./components/Canchita";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={(props) => <GridCanchita />} />
+        <Route
+          path="/canchita/:id"
+          component={(props) => <Canchita {...props} />}
+        />
+
+        <Route>404 Page </Route>
+      </Switch>
+    </Router>
   );
 }
 
