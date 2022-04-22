406b61b32d530657c2645ef9768e6b011f7d2e85
Jesus <jveracal553@gmail.com>
2022-04-22 15:00:35 -0500

    changing button submit owner

--- src/Components/GridOwner/index.js
+++ src/Components/GridOwner/index.js
@@ -7,43 +7,42 @@ import {
   SimpleGrid,
   Spacer,
   Stack,
-} from '@chakra-ui/react';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const GridCanchitaItem = ({ canchita }) => {
   return (
-    <div className='gridCanchitas'>
+    <div className="gridCanchitas">
       <Box
         p={4}
-        borderRadius='lg'
-        borderWidth='2px'
+        borderRadius="lg"
+        borderWidth="2px"
         _hover={{
-          background: 'white',
-          color: 'teal.500',
-          'box-shadow': '0 0 5px 5px #ddd',
+          background: "white",
+          color: "teal.500",
+          "box-shadow": "0 0 5px 5px #ddd",
         }}
       >
         <Box>
           <Center>
-              <Image mt={1} src={canchita.image} w={'95%'} h={'95%'}></Image>
+            <Image mt={1} src={canchita.image} w={"95%"} h={"95%"}></Image>
           </Center>
         </Box>
         <Box></Box>
-        <Heading mt={4} noOfLines={1} size='xs' fontWeight='Bold'>
+        <Heading mt={4} noOfLines={1} size="xs" fontWeight="Bold">
           {canchita.name}
         </Heading>
         <Spacer />
         <Box>
-          <Heading mt={4} noOfLines={1} size='xs' fontWeight='Normal'>
+          <Heading mt={4} noOfLines={1} size="xs" fontWeight="Normal">
             {canchita.direction}
           </Heading>
         </Box>
         <Stack>
           <Box>
-            <Heading mt={4} noOfLines={1} size='xs' fontWeight='Normal'>
+            <Heading mt={4} noOfLines={1} size="xs" fontWeight="Normal">
               Desde S/{canchita.price}
             </Heading>
           </Box>
@@ -53,16 +52,16 @@ const GridCanchitaItem = ({ canchita }) => {
   );
 };
 
-
-
-function GridOwner({url = 'http://localhost:3001/api/soccerField'}) {
+function GridOwner({ url = "http://localhost:3001/api/soccerField" }) {
   const [gridItem, setGridItem] = useState([]);
 
   useEffect(() => {
     axios
-      .get(url,{ headers: {
-          'Authorization': 'Bearer ' + localStorage.getItem('token')
-        }})
+      .get(url, {
+        headers: {
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
       .then(({ data }) => {
         setGridItem(data);
       });
@@ -70,18 +69,19 @@ function GridOwner({url = 'http://localhost:3001/api/soccerField'}) {
 
   return (
     <Box p={20}>
-        <Box p={4}>
-          <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} mt={0}>
-            {gridItem.map((item) => {
-              return <GridCanchitaItem key={item.id} canchita={item} />;
-            })}
-          </SimpleGrid>
-        </Box>
       <Box p={4}>
-        <Link to={'/registercanchita'}>Agregar canchita</Link>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} mt={0}>
+          {gridItem.map((item) => {
+            return <GridCanchitaItem key={item.id} canchita={item} />;
+          })}
+        </SimpleGrid>
+      </Box>
+      <Box p={4}>
+        <Button color="#42A692">
+          <Link to={"/registercanchita"}>Agregar canchita</Link>
+        </Button>
       </Box>
     </Box>
-
   );
 }
 
