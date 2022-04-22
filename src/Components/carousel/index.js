7ac8e0b7588cf0d45456fb123c3887580151806f
Raul Santa Cruz <raulsantacruz0@gmail.com>
2022-04-22 16:45:58 -0500

    fixing images

--- src/Components/carousel/index.js
+++ src/Components/carousel/index.js
@@ -3,6 +3,10 @@ import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import { Image } from "@chakra-ui/react";
+import caro1 from '../../images/caro1.jpeg'
+import caro2 from '../../images/caro2.jpeg'
+import caro3 from '../../images/caro3.jpeg'
+
 
 class ImageGallaryComponent extends React.Component {
   render() {
@@ -11,25 +15,27 @@ class ImageGallaryComponent extends React.Component {
         <Carousel autoPlay interval="5000" transitionTime="5000">
           <div>
             <Image
-              h={"70%"}
-              w={"90%"}
-              src="https://picsum.photos/700/400?img=1"
+              h={"150px"}
+              w={"90px"}
+              src={caro1}
             />
             {/* <p className="legend">My Classic Still 1</p> */}
           </div>
-          <div>
+          <div style={{"width": "500px",
+                        "height": "300px"
+        }}>
             <Image
-              h={"70%"}
-              w={"90%"}
-              src="https://picsum.photos/700/400?img=2"
+              h={"200px"}
+              w={"90px"}
+              src={caro2}
             />
             {/* <p className="legend">My Classic Still 2</p> */}
           </div>
           <div>
             <Image
-              h={"70%"}
-              w={"90%"}
-              src="https://picsum.photos/700/400?img=3"
+              h={"70px"}
+              w={"90px"}
+              src={caro3}
             />
             {/* <p className="legend">My Classic Still 3</p> */}
           </div>
