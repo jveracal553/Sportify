64c07c367cd23339e9c7dc36acefa7abaae859c8
Raul Santa Cruz <raulsantacruz0@gmail.com>
2022-04-22 11:35:56 -0500

    update hours

--- src/Components/GroupHour/index.js
+++ src/Components/GroupHour/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hour from '../Hour'
 
 function GroupHour({hours}) {
+    console.log('aca algo', hours)
     return (
         hours.map(hour=> <Hour hour={hour}/>)
     );
--- src/Components/Hour/index.js
+++ src/Components/Hour/index.js
@@ -1,15 +1,29 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext, useRef } from 'react'
+import {useHistory} from 'react-router-dom'
+import { UserContext } from "../../context/userContext";
 import styled from 'styled-components'
 
 const Contenedor = styled.div`
-width: 30vw;
+display: flex;
+flex-direction: column;
+`
+
+const Accept = styled.button`
+background: #42a692;
+border-radius: 25px;
+padding: 10px 15px;
+margin: 10px;
+margin-top: 20vh;
+`
+
+const Hours = styled.div`
+width: 50vw;
 display: flex;
 flex-direction: row;
 flex-wrap: wrap;
 align-items: center;
-justify-content: center;
-border: 1px solid #000;
-`
+justify-content: center;`
+
 
 const Button = styled.button`
 border: none;
@@ -21,7 +35,7 @@ background: ${props => props.type==='busy'? "#333" : props.type==='availableSele
 `
 
 const getAll = async(data)=>{
-    let response = await fetch('http://localhost:3001/api/reservation', {
+    let response = await fetch(`http://localhost:3001/api/reservation/`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(data),
@@ -30,29 +44,51 @@ const getAll = async(data)=>{
     return response
 }
 
-const Array =[
-    {hour: 10, state: 'busy'},
-    {hour: 11, state: 'busy'},
-    {hour: 12, state: 'availableNoSelected'},
-    {hour: 13, state: 'availableNoSelected'},
-    {hour: 14, state: 'availableNoSelected'},
-    {hour: 15, state: 'busy'},
-    {hour: 16, state: 'availableNoSelected'},
-    {hour: 17, state: 'availableNoSelected'},
-    {hour: 18, state: 'availableNoSelected'},
-    {hour: 19, state: 'availableNoSelected'},
-    {hour: 20, state: 'availableNoSelected'},
-    {hour: 21, state: 'availableNoSelected'},
-    {hour: 22, state: 'busy'}]
-
 
 export default function Hour ({day, month, year, soccerFieldId}) {
-    const [hour, setHour] = useState(Array)
+    const mountedRef = useRef(true)
+    const [hour, setHour] = useState([])
+    const history = useHistory()
     const [selection, setSelection] = useState([])
+    const { user } = useContext(UserContext);
     useEffect(()=>{
-        const h = getAll({day, month, year, soccerFieldId})
-    })
+        getAll({day, month, year, soccerFieldId: soccerFieldId.id})
+        .then(data=>{
+            setHour(data)
+        })
+        return () => { mountedRef.current = false }
+    },[day, month, year, soccerFieldId])
+    const cancel = () =>{
+        history.push('/')
+    }
+    const confirmar = async () =>{
+        if(selection.length ===0){
+            alert('no seas vivo selecciona algo')
+        }else{
+            const data = []
+            for(let el of selection ){
+                let aux = {day, month, year}
+                aux.time = el
+                data.push(aux)
+            }
+            const response = await fetch(`http://localhost:3001/api/reservation/create/${soccerFieldId.id}`, {
+                method: 'POST',
+                headers: { 
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${localStorage.getItem('token')}`
+                 },
+                body: JSON.stringify({data}),
+              });
+              const resjson = await response.json()
+              console.log(resjson)
+              alert(resjson.sucess)
+              history.push('/')
+        }
+    }
     const selec = (x)=>{
+        if(user.name === ""){
+            history.push('/login/user')
+        }
         if(x.state !== 'busy'){
             if(x.state === 'availableNoSelected'){ 
                 setSelection([...selection, x.hour])
@@ -76,10 +112,17 @@ export default function Hour ({day, month, year, soccerFieldId}) {
         }
     }
   return (
-    <Contenedor>
-      {
-        hour.map((x,i) => <Button key={i} type={x.state} onClick={()=>selec(x)}>{x.hour}</Button>)
-      }
-    </Contenedor>
+      <Contenedor>
+        <Hours>
+        {
+            
+            hour.map((x,i) => <Button key={i} type={x.state} onClick={()=>selec(x)}>{x.hour}</Button>)
+        }   
+        </Hours>
+        <Hours>
+            <Accept onClick={cancel}>Cancelar</Accept>
+            <Accept onClick={confirmar}>Confirmar</Accept> 
+        </Hours>        
+      </ Contenedor>
   )
 }
