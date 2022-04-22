654389554a9b956f6490fe4a6b0dbfba9bf3d83d
Jesus <jveracal553@gmail.com>
2022-04-22 15:12:31 -0500

    styling buttons login

--- src/Components/SelecLogin/index.js
+++ src/Components/SelecLogin/index.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import soccer from '../../images/soccer.jpg';
-import './SelecLogin.css';
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  Image,
+  SimpleGrid,
+  Spacer,
+  Stack,
+} from "@chakra-ui/react";
+import React from "react";
+// import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import soccer from "../../images/soccer.jpg";
+import "./SelecLogin.css";
 
 function SelecLogin() {
-    return (
-        <div className='register'>
-            <div className='register-text'>
-                <div className='register-text__logo'>
-                    <i className='fas fa-futbol'></i>
-                    <Link to={'/'} className='register-text__logo-name color'>Sportify</Link>
-                </div>
-                <div><Link to={'/login/owner'} className='register-text__logo link color'>Soy un Propietario</Link></div>
-                <div><Link to={'/login/user'} className='register-text__logo link color'>Soy un Usuario</Link></div>
-                
-            </div>
-            <div className='register-image'>
-                <img className='soccer' src={soccer} alt='' />
-            </div>
+  return (
+    <div className="register">
+      <div className="register-text">
+        <div className="register-text__logo">
+          <i className="fas fa-futbol"></i>
+          <Link to={"/"} className="register-text__logo-name color">
+            Sportify
+          </Link>
         </div>
-    );
+        <div>
+          {/* <Button color="42A692">
+            <Link to={"/login/owner"}>Soy un Propietarios</Link>
+          </Button> */}
+
+          <SimpleGrid p={4} spacing={10}>
+            <Button color="#42A692">
+              <Link to={"/login/owner"}>Propietario</Link>
+            </Button>
+            <Button color="#42A692">
+              <Link to={"/login/user"}>Soy un Usuario</Link>
+            </Button>
+          </SimpleGrid>
+        </div>
+        {/* <div>
+          <Link to={"/login/user"} className="register-text__logo link color">
+            Soy un Usuario
+          </Link>
+        </div> */}
+      </div>
+      <div className="register-image">
+        <img className="soccer" src={soccer} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default SelecLogin;
